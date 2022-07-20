767029412964d9ebe5d0cb67cb200aff01db890c
Abdullah Alrayes <a.alrayes.404@gmail.com>
2022-07-20 18:55:33 +0300

    Update readFiles.js

--- util/readFiles.js
+++ util/readFiles.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
-module.exports = function readFromFile(file,sperator) {
+module.exports = function readFromFile(fileRoute,sperator) {
   try {
-    const data = fs.readFileSync(file, "utf8");
+    const data = fs.readFileSync(fileRoute, "utf8");
 
     return data.split(sperator);
   } catch (err) {
